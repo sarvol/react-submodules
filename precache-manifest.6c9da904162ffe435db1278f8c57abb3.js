b40e2d8d1f139919f84b6b65e465d454e775bca1
sarvol <savo1009@gmail.com>
2019-11-27 11:21:10 +0100

    Updates

---
+++ precache-manifest.6c9da904162ffe435db1278f8c57abb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d2972f97cb1b66f007e3480c5a6b6f",
+    "url": "/react-submodules/index.html"
+  },
+  {
+    "revision": "04c9fa9a589c157f1c37",
+    "url": "/react-submodules/static/css/main.de9d0ec4.chunk.css"
+  },
+  {
+    "revision": "da8af002cc14d55d6177",
+    "url": "/react-submodules/static/js/2.a922cbb3.chunk.js"
+  },
+  {
+    "revision": "04c9fa9a589c157f1c37",
+    "url": "/react-submodules/static/js/main.ad4be1c8.chunk.js"
+  },
+  {
+    "revision": "c900d3900270f3cc22a9",
+    "url": "/react-submodules/static/js/runtime-main.6779e2cf.js"
+  }
+]);
